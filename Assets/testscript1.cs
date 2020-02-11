34fd8b42e0c7fb24546a0c8ee5efd21586bf5867
sgullapalli13 <sgullapalli13@gmail.com>
2020-02-11 16:28:34 -0500

    testing shreya sphere commit
    testing shreya sphere commit

---
+++ Assets/testscript1.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testscript1 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown("1")){
+            print("script works");
+        }
+        
+    }
+}
