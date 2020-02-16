0ab5785e207f66c97f08382dc81886de04e900c5
rammyc <racmpc@gmail.com>
2020-02-16 13:17:05 -0500

    Implemented most fundamental mechanics
    All changes are on the JeaneMechanicsTest scene. Most of the basic things have been implemented besides inventory stuff.

---
+++ Assets/Scripts/CustomCursor.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CustomCursor : MonoBehaviour
+{
+    //NOTE: Must be attached to an object named "Cursor"!
+
+    //The text UI object attached to the cursor.
+    private Text cursorText;
+    private Text descriptionText;
+
+    void Start() {
+        Cursor.visible = false;
+        cursorText = GetComponentInChildren<Text>();
+        cursorText.text = "";
+        descriptionText = GameObject.Find("Description").GetComponent<Text>();
+    }
+
+    void Update() {
+        //Cursor image follows real mouse position.
+        transform.position = Input.mousePosition;
+    }
+
+    public void setCursorText(string t) {
+        cursorText.text = t;
+    }
+
+    public void setDescriptionText(string t) {
+        descriptionText.text = t;
+    }
+}
---
+++ Assets/Scripts/Interactable.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactable : MonoBehaviour
+{
+    public string name; //Object name; shown on hover.
+    public string description; //Object description; shown on click if item can't be picked up. 
+    public bool canBeTaken; //Whether or not the object can be picked up.
+    public Item item; //The item added to the inventory when the object is picked up.
+    public bool destroyWhenTaken = true; //Whether or not the gameobject will be destroyed when the item is picked up.
+
+    //the CustomCursor in the scene; should only be one.
+    private CustomCursor cc;
+
+    void Start() {
+        cc = GameObject.Find("Cursor").GetComponent<CustomCursor>();
+    }
+
+    void OnMouseOver() {
+        //Sets the cursor text to this object's name when hovered over
+        cc.setCursorText(name);
+
+        //On click...
+        if (Input.GetMouseButtonDown(0)) {
+            if (canBeTaken) { 
+                TakeItem();
+                //Set the description to be blank
+                cc.setDescriptionText(""); 
+
+                //if the object is destroyed when taken, destroy it. Otherwise, just make it so you can't take it again.
+                if (destroyWhenTaken) {
+                    cc.setCursorText("");
+                    Object.Destroy(gameObject); 
+                }
+                else { canBeTaken = false; }
+            }
+            //If this object can't be taken, put the description text in the box
+            else { cc.setDescriptionText(description); }
+        }
+    }
+
+    void OnMouseExit() {
+        //When the mouse is no longer hovering over this object, empty the cursor text
+        cc.setCursorText("");
+    }
+    
+    void TakeItem() {
+        //placeholder
+    }
+}
