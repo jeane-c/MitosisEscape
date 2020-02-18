62217706e2789f850abe172a905f676e9b596dc1
Jeffrey Chen <jeffcc@live.unc.edu>
2020-02-18 16:37:28 -0500

    Camera Movement

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    float inputX, inputZ;
+    // Update is called once per frame
+    void Update()
+    {
+        inputX = Input.GetAxis("Horizontal");
+        inputZ = Input.GetAxis("Vertical");
+        
+        if (inputX != 0)
+            rotate();
+        if (inputZ != 0)
+            move();
+
+      
+
+
+
+    }
+    private void rotate()
+    {
+        int degree = 0;
+        //transform.Rotate(new Vector3(0f, degree + 90, 0f));
+
+        transform.Rotate(new Vector3(0f, inputX * Time.deltaTime, 0f));
+        //Camera.main.transform.Rotate(new Vector3(0, 90, 0));
+    }
+
+    private void move()
+    {
+        transform.position += transform.forward * inputZ * Time.deltaTime;
+    }
+}
