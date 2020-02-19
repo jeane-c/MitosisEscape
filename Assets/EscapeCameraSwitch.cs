deecb6c3d35b2dcf7884098627f58960b2f79acd
Jacob Yang <jyy@live.unc.edu>
2020-02-19 11:59:02 -0500

    CameraSwitching

---
+++ Assets/EscapeCameraSwitch.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EscapeCameraSwitch : MonoBehaviour
+{
+    public GameObject Wall5;
+    public GameObject Wall2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+         if(Input.GetKeyDown("1")){
+            print("script works");
+        }
+         if(Input.GetKeyDown("2")){
+            Wall5.SetActive(true);
+            Wall2.SetActive(false);
+        }
+          if(Input.GetKeyDown("3")){
+            Wall2.SetActive(true);
+            Wall5.SetActive(false);
+        }
+    }
+}
