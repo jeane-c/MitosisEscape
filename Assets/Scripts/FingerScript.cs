b3bc204555a42117c9f4299a93ed1b0e6bed4b23
rammyc <racmpc@gmail.com>
2020-02-20 23:09:10 -0500

    pull

---
+++ Assets/Scripts/FingerScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FingerScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown("1")){
+            print("script works");
+        }
+         if(Input.GetKeyDown("2")){
+            print("2 button hit");
+        }
+    }
+}
