c18acccfc5fe8b5a90129eea32f32b1b5c71ea75
sgullapalli13 <sgullapalli13@gmail.com>
2020-02-26 14:17:50 -0500

    hhhh

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -19,30 +19,33 @@ public class CameraMovement : MonoBehaviour
         inputX = Input.GetAxis("Horizontal");
         inputZ = Input.GetAxis("Vertical");
         
-       // if (inputX != 0)
-       //     rotate();
-       // if (inputZ != 0)
-       //     move();
+       if (inputX != 0)
+            rotate();
+       if (inputZ != 0)
+            move();
 
-        if (Input.GetKeyDown(KeyCode.LeftArrow))
-            transform.Rotate(new Vector3(0f, degree - 90, 0f));
+       // if (Input.GetKeyDown(KeyCode.LeftArrow))
+            // transform.Rotate(new Vector3(0f, degree - 90, 0f));
 
-        if (Input.GetKeyDown(KeyCode.RightArrow))
-            transform.Rotate(new Vector3(0f, degree + 90, 0f));
+       // if (Input.GetKeyDown(KeyCode.RightArrow))
+            // transform.Rotate(new Vector3(0f, degree + 90, 0f));
 
 
     }
     private void rotate()
     {
-        int degree = 0;
-        //transform.Rotate(new Vector3(0f, degree + 90, 0f));
+        // int degree = 0;
+        // transform.Rotate(new Vector3(0f, degree + 90, 0f));
 
-        transform.Rotate(new Vector3(0f, inputX * Time.deltaTime, 0f));
-        //Camera.main.transform.Rotate(new Vector3(0, 90, 0));
+
+        transform.Rotate(new Vector3(0f, inputX * Time.deltaTime * 100, 0f));
+        // Camera.main.transform.Rotate(new Vector3(0, 90, 0));
     }
 
     private void move()
     {
         transform.position += transform.forward * inputZ * Time.deltaTime;
     }
+
+
 }
---
+++ Assets/Scripts/EscapeCameraSwitch.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EscapeCameraSwitch : MonoBehaviour
+{
+    public GameObject Cam1;
+    public GameObject Cam2;
+    public GameObject Cam3;
+    public GameObject Cam4;
+     public GameObject Cam5;
+     public GameObject DeskView;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+         if(Input.GetKeyDown("1")){
+           Cam1.SetActive(true);
+            Cam2.SetActive(false);
+            Cam3.SetActive(false);
+            Cam4.SetActive(false);
+               Cam5.SetActive(false);
+               DeskView.SetActive(false);
+        }
+         if(Input.GetKeyDown("2")){
+           
+            Cam1.SetActive(false);
+            Cam2.SetActive(true);
+            Cam3.SetActive(false);
+            Cam4.SetActive(false);
+               Cam5.SetActive(false);
+                DeskView.SetActive(false);
+        }
+          if(Input.GetKeyDown("3")){
+            Cam1.SetActive(false);
+            Cam2.SetActive(false);
+            Cam3.SetActive(true);
+             Cam4.SetActive(false);
+                Cam5.SetActive(false);
+                 DeskView.SetActive(false);
+          
+        }
+         if(Input.GetKeyDown("4")){
+            Cam1.SetActive(false);
+            Cam2.SetActive(false);
+            Cam3.SetActive(false);
+             Cam4.SetActive(true);
+              Cam5.SetActive(false);
+               DeskView.SetActive(false);
+            
+        }
+         if(Input.GetKeyDown("5")){
+           Cam1.SetActive(false);
+            Cam2.SetActive(false);
+            Cam3.SetActive(false);
+             Cam4.SetActive(false);
+             Cam5.SetActive(true);
+              DeskView.SetActive(false);
+            
+        }
+             if(Input.GetKeyDown("6")){
+            
+             Cam1.SetActive(false);
+            Cam2.SetActive(false);
+            Cam3.SetActive(false);
+             Cam4.SetActive(false);
+             Cam5.SetActive(false);
+              DeskView.SetActive(true);
+        }
+    }
+}
