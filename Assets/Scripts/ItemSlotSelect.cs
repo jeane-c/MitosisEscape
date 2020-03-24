92c9a4634077573c7e58d18624f8df3e8948a2fd
rammyc <racmpc@gmail.com>
2020-03-24 19:01:35 -0400

    Implemented full item usage functionality!

---
+++ Assets/Scripts/ItemSlotSelect.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ItemSlotSelect : MonoBehaviour, IPointerClickHandler
+{
+    //the inventory; should also only be one
+    private Inventory inv;
+    public int selectID = 0;
+
+    // Start is called before the first frame update
+    void Start() {
+        inv = GameObject.Find("Inventory").GetComponent<Inventory>();
+    }
+
+    public void OnPointerClick(PointerEventData e) {
+        inv.setSelectedItemIndex(selectID);
+        Debug.Log("Clicked item slot " + selectID);
+    }
+}
