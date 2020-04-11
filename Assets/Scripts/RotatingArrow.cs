5ac635e664fe72d89dab67ca0ace3a237291b9c3
Jeffrey Chen <jeffcc@live.unc.edu>
2020-04-11 13:12:01 -0400

    Arrow stops rotating on click

--- Assets/Scripts/RotatingArrow.cs
+++ Assets/Scripts/RotatingArrow.cs
@@ -7,12 +7,19 @@ public class RotatingArrow : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
-    // Update is called once per frame
+    public bool rotateObject; 
+
     void Update()
     {
-        
+        if (rotateObject) { 
+            transform.Rotate(new Vector3(0f, Time.deltaTime * 500, 0f));
+        } else
+        {
+            Debug.Log("stop please");
+        }
     }
 }
+
---
+++ Assets/Scripts/StopRotation.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StopRotation : MonoBehaviour
+{
+    public RotatingArrow arrow;
+
+    void Start()
+    {
+        arrow = GetComponentInChildren<RotatingArrow>();
+        arrow.rotateObject = true;
+    }
+
+
+    void OnMouseOver()
+    {
+        Debug.Log("hovering");
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("is this working?");
+            arrow.rotateObject = false;
+
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
