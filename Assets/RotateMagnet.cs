d9f54c6c5a05553a5ab5a7165e5b7a1c4906de48
Jacob Yang <jyy@live.unc.edu>
2020-04-20 14:53:27 -0400

    Magnet Changes

---
+++ Assets/RotateMagnet.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RotateMagnet : MonoBehaviour
+{
+
+    public GameObject m1, m2, m3, m4, m5;
+    public GameObject button1;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+ 
+    // Update is called once per frame
+    void Update()
+    {
+         if(Input.GetKeyDown("1")){     
+              m1.transform.Rotate(0, 180, 0, Space.World);
+        }
+         if(Input.GetKeyDown("2")){     
+              m2.transform.Rotate(0, 180, 0, Space.World);
+        }
+         if(Input.GetKeyDown("3")){     
+              m3.transform.Rotate(0, 180, 0, Space.World);
+        }
+         if(Input.GetKeyDown("4")){     
+              m4.transform.Rotate(0, 180, 0, Space.World);
+         }
+         if(Input.GetKeyDown("5")){     
+              m5.transform.Rotate(0, 180, 0, Space.World);
+        }
+
+    }
+
+    
+       void RotateMagnets(){
+       
+    }
+
+}
---
+++ Assets/Scripts/MagnetRotate.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MagnetRotate : MonoBehaviour
+{
+    public GameObject m1, m2, m3, m4, m5;
+    Ray ray;
+    RaycastHit hit;
+     
+     void Update()
+     {
+            if(Input.GetMouseButtonDown(0)){
+                ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                 if(Physics.Raycast(ray, out hit))
+                 {
+                    if (hit.collider.name == "MButton1"){
+                          m1.transform.Rotate(0, 180, 0, Space.World);
+                        m4.transform.Rotate(0, 180, 0, Space.World);
+                          m5.transform.Rotate(0, 180, 0, Space.World);
+                    }
+                      if (hit.collider.name == "MButton2"){
+                          m2.transform.Rotate(0, 180, 0, Space.World);
+                         m3.transform.Rotate(0, 180, 0, Space.World);
+                    }
+                     if (hit.collider.name == "MButton3"){
+                          m2.transform.Rotate(0, 180, 0, Space.World);
+                            m5.transform.Rotate(0, 180, 0, Space.World);
+                    
+                    }
+                }
+            }
+        
+     }
+}
