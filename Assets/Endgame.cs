7cb35ea3a376f2dba452378065b06b456fed974e
sgullapalli13 <sgullapalli13@gmail.com>
2020-04-28 07:43:27 -0400

    win

---
+++ Assets/Endgame.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Endgame : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject.Find("wintext").SetActive(true);
+    }
+
+ 
+}
