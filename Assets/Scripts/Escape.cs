fce72afc77d234ed0a2b4dac73aca3ceaecc986a
sgullapalli13 <sgullapalli13@gmail.com>
2020-05-01 07:23:29 -0400

    escape is obvious now

---
+++ Assets/Scripts/Escape.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Escape : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject.Find("door_01").transform.Rotate(0.0f, 90.0f, 0.0f, Space.Self);
+        GameObject.Find("door_02").transform.Rotate(0.0f, -90.0f, 0.0f, Space.Self);
+
+        
+    }
+
+    // Update is called once per frame
+    
+}
